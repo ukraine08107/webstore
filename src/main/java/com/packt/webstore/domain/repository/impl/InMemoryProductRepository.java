9a5c39cb49a28f16f70e30ff621467a6bbde86c8
Sobianin <sobianin2008@gmail.com>
2021-01-22 23:15:27 +0200

    add sorting feature by all arguments

--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -18,7 +18,7 @@ import com.packt.webstore.domain.repository.ProductRepository;
             iphone.setManufacturer("Apple");
             iphone.setUnitsInStock(1000);
 
-            Product laptop_dell = new Product("P1235","Dell Inspiron", new BigDecimal(700));
+            Product laptop_dell = new Product("P1235","Dell Inspiron", new BigDecimal(800));
             laptop_dell.setDescription("Dell Inspiron 14-inch Laptop(Black) with 3rd Generation Intel Core processors");
             laptop_dell.setCategory("Laptop");
             laptop_dell.setManufacturer("Dell");
@@ -26,13 +26,20 @@ import com.packt.webstore.domain.repository.ProductRepository;
 
             Product tablet_Nexus = new Product("P1236","Nexus 7", new BigDecimal(300));
             tablet_Nexus.setDescription("Google Nexus 7 is the lightest7 inch tablet With a quad-core Qualcomm Snapdragon™ S4 Proprocessor");
-                    tablet_Nexus.setCategory("Tablet");
+            tablet_Nexus.setCategory("Tablet");
             tablet_Nexus.setManufacturer("Google");
             tablet_Nexus.setUnitsInStock(1000);
+
+            Product laptop_google = new Product("P1237","Google Inspiron", new BigDecimal(666));
+            laptop_google.setDescription("Google Laptop(Black");
+            laptop_google.setCategory("Laptop");
+            laptop_google.setManufacturer("Google");
+            laptop_google.setUnitsInStock(1000);
 
             listOfProducts.add(iphone);
             listOfProducts.add(laptop_dell);
             listOfProducts.add(tablet_Nexus);
+            listOfProducts.add(laptop_google);
         }
         public List<Product> getAllProducts() {
             return listOfProducts;
@@ -67,134 +74,53 @@ import com.packt.webstore.domain.repository.ProductRepository;
             return productsByCategory;
 
         }
-//        public Set<Product> getProductsByFilter(Map<String, List<String>>filterParams) {
-//            Set<Product> productsByBrand = new HashSet<Product>();
-//            Set<Product> productsByCategory = new HashSet<Product>();
-//            Set<Product> productsByPrice = new HashSet<Product>();
-//            Set<String> criterias = filterParams.keySet();
-//
-//            if(criterias.contains("brand")) {
-//                for(String brandName: filterParams.get("brand")) {
-//                    for(Product product: listOfProducts) {
-//                        if(brandName.equalsIgnoreCase(product.getManufacturer())){
-//                            productsByBrand.add(product);
-//                        }
-//                    }
-//                }
-//            }
-//
-//            if(criterias.contains("category")) {
-//                for (String category : filterParams.get("category")) {
-//                    productsByCategory.addAll(this.getProductsByCategory(category));
-//                }
-//            }
-//
-//            if(criterias.contains("price")) {
-//                    for(String priceString: filterParams.get("unitPrice")) {
-//                        for (Product product:listOfProducts) {
-//                            if (product.getUnitPrice().toString().equalsIgnoreCase(priceString)){
-//                                productsByPrice.add(product);
-//                            }
-//                        }
-//
-//                    }
-//            }
-//            productsByCategory.retainAll(productsByBrand);
-//            //productsByCategory.addAll(productsByPrice);
-//            return productsByCategory;
-//
-//                }
-
         public List<Product> getProductsByManufacturer(String manufacturer) {
             return null;
         }
 public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams) {
-
-    Set<Product> sorted = new HashSet<Product>() ;
-    sorted.addAll(listOfProducts);
-
-    List<Product> listToBeDeleted = new ArrayList<Product>();
-
+    Set<Product> addedProducts = new HashSet<Product>();
+    addedProducts.addAll(listOfProducts);
     Set<String> criterias = filterParams.keySet();
 
     if (criterias.contains("brand")) {
+        Set<Product> sorted = new HashSet<Product>() ;
         for (String brandName : filterParams.get("brand")) {
-            for (Product product : sorted) {
-                if (!brandName.equalsIgnoreCase(product.getManufacturer())) {
-                    //sorted.remove(product);
-                    listToBeDeleted.add(product);
-                    System.out.println(product.toString());
+            for (Product product : listOfProducts) {
+                if (brandName.equalsIgnoreCase(product.getManufacturer())) {
+                    sorted.add(product);
                 }
             }
         }
+        addedProducts.retainAll(sorted);
     }
-sorted.removeAll(listToBeDeleted);
-    listToBeDeleted = new ArrayList<Product>();
-
     if (criterias.contains("category")) {
+        Set<Product> sorted = new HashSet<Product>() ;
         for (String category : filterParams.get("category")) {
-            // productsByCategory.addAll(this.getProductsByCategory(category));
-            for (Product product : sorted) {
-                if (!category.equalsIgnoreCase(product.getCategory())) {
-                    listToBeDeleted.add(product);
+            for (Product product : listOfProducts) {
+                if (category.equalsIgnoreCase(product.getCategory())) {
+                    sorted.add(product);
                 }
             }
         }
+        addedProducts.retainAll(sorted);
     }
-    sorted.removeAll(listToBeDeleted);
-    listToBeDeleted = new ArrayList<Product>();
-
     if (criterias.contains("price")) {
+    ArrayList<String> prices = new ArrayList<String>();
         for (String priceString : filterParams.get("price")) {
-            for (Product product : sorted) {
-                if (!product.getUnitPrice().toString().equalsIgnoreCase(priceString)) {
-                    listToBeDeleted.add(product);
-
-                }
-            }
-
+        prices.add(priceString);
         }
+    BigDecimal min = new BigDecimal(prices.get(0));
+        BigDecimal max = new BigDecimal(prices.get(1));
 
+        Set<Product> sorted = new HashSet<Product>() ;
+            for (Product product : listOfProducts) {
+                if (product.getUnitPrice().compareTo(min)>=0 && product.getUnitPrice().compareTo(max)<=0){
+                    sorted.add(product);
+                }
+                }
+
+        addedProducts.retainAll(sorted);
     }
-    sorted.removeAll(listToBeDeleted);
-//        productsByCategory.retainAll(productsByBrand);
-//        //productsByCategory.addAll(productsByPrice);
-
-    return sorted;
-
-}
-
-
-
-//        public List<Product> getProductsByManufacturer(String manufacturer) {
-//            List<Product> productsByManufacturer = new ArrayList<Product>();
-//            for (Product product : getAllProducts()) {
-//                if (product.getManufacturer().equalsIgnoreCase(manufacturer)) {
-//                    productsByManufacturer.add(product);
-//                }
-//            }
-
-
-
-
-
-//            List<Product> productsByCategory = new ArrayList<Product>();
-//
-//            for(Product product: listOfProducts) {
-//                if(manufacturer.equalsIgnoreCase(product.getManufacturer())){
-//productsByCategory.retainAll(productsByManufacturer);
-//                }
-//            }
-//            return productsByCategory;
-//            List<Product> productsBуManufacturer= new ArrayList<Product>();
-//
-//            for(Product product: listOfProducts) {
-//                if(manufacturer.equalsIgnoreCase(product.getManufacturer())){
-//                    productsBуManufacturer.add(product);
-//                }
-//            }
-//            return productsBуManufacturer;
-
-//            return productsByManufacturer;
-//        }
+    return addedProducts;
+        }
 }
